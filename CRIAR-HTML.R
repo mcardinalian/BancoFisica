e230b4584d9fe23447d6c082dd2ca6ca622b15ec
mcardinalian <mcardinalian@gmail.com>
2017-06-27 21:56:36 -0300

    numeração de questões; atualização gera-html

--- CRIAR-HTML.R
+++ CRIAR-HTML.R
@@ -13,7 +13,6 @@ exams2html(myexam, n = 1,solution=TRUE,
            edir = "./BancoDeQuestoes/exemplos",
            template = "templates/plain.html")
 
-
 ############### velocidade media ##################
 ## Definindo a pasta com as questoes do exame
 myexam <- dir("./BancoDeQuestoes/vm/")
@@ -25,7 +24,6 @@ exams2html(myexam, n = 1,solution=TRUE,
   edir = "./BancoDeQuestoes/vm",
   template = "templates/plain.html")
 
-
 ############### aceleração media ##################
 ## Definindo a pasta com as questoes do exame
 myexam <- dir("./BancoDeQuestoes/acel/")
@@ -36,4 +34,39 @@ exams2html(myexam, n = 1,solution=TRUE,
            encoding = "UTF-8",
            edir = "./BancoDeQuestoes/acel",
            template = "plain8")
+
+############### calor e temperatura ##################
+## Definindo a pasta com as questoes do exame
+myexam <- dir("./BancoDeQuestoes/calortemp/")
+
+## Gerando HTML com o arquivo da questao
+set.seed(12017)
+exams2html(myexam, n = 1,solution=TRUE,
+           encoding = "UTF-8",
+           edir = "./BancoDeQuestoes/calortemp",
+           template = "templates/plain.html")
+
+
+############### calorimetria ##################
+## Definindo a pasta com as questoes do exame
+myexam <- dir("./BancoDeQuestoes/calorimetria/")
+
+## Gerando HTML com o arquivo da questao
+set.seed(12017)
+exams2html(myexam, n = 1,solution=TRUE,
+           encoding = "UTF-8",
+           edir = "./BancoDeQuestoes/calorimetria",
+           template = "templates/plain.html")
+
+
+############### dilatação térmica ##################
+## Definindo a pasta com as questoes do exame
+myexam <- dir("./BancoDeQuestoes/dilatterm/")
+
+## Gerando HTML com o arquivo da questao
+set.seed(12017)
+exams2html(myexam, n = 1,solution=TRUE,
+           encoding = "UTF-8",
+           edir = "./BancoDeQuestoes/dilatterm",
+           template = "templates/plain.html")
 
