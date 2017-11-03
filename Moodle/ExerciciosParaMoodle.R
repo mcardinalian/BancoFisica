59cfb592574765b87bafbaddf2fe83091376fdf7
atTh3SK <th3solrac@gmail.com>
2017-11-03 14:48:22 -0200

    correções

--- Moodle/ExerciciosParaMoodle.R
+++ Moodle/ExerciciosParaMoodle.R
@@ -70,3 +70,40 @@ exams2moodle(myexam, n = 50, rule="none", schoice = list(shuffle = TRUE), name =
              encoding = "UTF-8",
              dir = "./Moodle",
              edir = "./BancoDeQuestoes/acel")
+
+############### dilata??o trmica ##################
+## Definindo a pasta com as questoes do exame
+myexam <- dir("./BancoDeQuestoes/dilatterm", pattern = ".rnw", ignore.case=TRUE)
+
+## Gerando xml com o arquivo da questao
+set.seed(12017)
+set.seed(ano)
+exams2moodle(myexam, n = 50, rule="none", schoice = list(shuffle = TRUE), name = paste0("dilatterm-",ano),
+             encoding = "UTF-8",
+             dir = "./Moodle",
+             edir = "./BancoDeQuestoes/dilatterm")
+
+############### termodinamica ##################
+## Definindo a pasta com as questoes do exame
+myexam <- dir("./BancoDeQuestoes/termodinamica", pattern = ".rnw", ignore.case=TRUE)
+
+## Gerando xml com o arquivo da questao
+set.seed(12017)
+set.seed(ano)
+exams2moodle(myexam, n = 50, rule="none", schoice = list(shuffle = TRUE), name = paste0("termd-",ano),
+             encoding = "UTF-8",
+             dir = "./Moodle",
+             edir = "./BancoDeQuestoes/termodinamica")
+
+############### eletromagnetismo ##################
+## Definindo a pasta com as questoes do exame
+myexam <- dir("./BancoDeQuestoes/eletromag", pattern = ".rnw", ignore.case=TRUE)
+
+## Gerando xml com o arquivo da questao
+set.seed(12017)
+## Cria o arquivo .xml para entrada no moodle
+set.seed(ano)
+exams2moodle(myexam, n = 1, rule="none", schoice = list(shuffle = TRUE), name = paste0("eletromag-",ano),
+             encoding = "UTF-8",
+             dir = "./Moodle",
+             edir = "./BancoDeQuestoes/eletromag")
